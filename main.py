de940a7a61f03337625c0c920c14c50589fcdd2d
porterclev <porterclev@gmail.com>
2026-04-30 13:58:28 -0700

    language model examples

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from src.vsm_search import VSMSearch
 from src.bm25_search import BM25Search
 from src.bim_search import BIMSearch
 
+# Language Models
+from src.language_model import LanguageModel
+
 def main():
     p = DataProcessing() # default datapath is "./dataset"
     
@@ -19,6 +22,8 @@ def main():
     bm25 = BM25Search(p.docs, p.qrels)
     vsm = VSMSearch(p.docs, p.qrels, p.vectorizer, p.tfidf)
     bim = BIMSearch(p.docs, p.qrels)
+    unigram = LanguageModel(docs=p.docs, relevance=p.qrels, model='unigram', lambda_=0.3)
+    bigram = LanguageModel(docs=p.docs, relevance=p.qrels, model='bigram', lambda_=0.3)
 
     sorted_bm25 = bm25.getAPScores(p.queries)
     print("====================================================")
@@ -38,5 +43,17 @@ def main():
     for q, ap in sorted_bim:
         print(f'{q}: {ap:.4f}')
         
+    sorted_unigram = unigram.getAPScores(p.queries)
+    print("====================================================")
+    print("Unigram Sorted :", end='\n\n')
+    for q, ap in sorted_unigram:
+        print(f'{q}: {ap:.4f}')
+
+    sorted_bigram = bigram.getAPScores(p.queries)
+    print("====================================================")
+    print("Bigram Sorted :", end='\n\n')
+    for q, ap in sorted_bigram:
+        print(f'{q}: {ap:.4f}')
+    
 if __name__ == "__main__":
     main()
