af6f24e2c698e0ad59ac856e48022cebbcef4190
porterclev <porterclev@gmail.com>
2026-04-30 12:52:37 -0700

    bm25 example

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from src.ProcessesData import ProcessesData
-
+from src.bm25_search import BM25Search
 
 def main():
     p = ProcessesData() # default datapath is "./dataset"
-    print(p.docs)       # gets documents in collection
-    print(p.qrels)      # gets relevance in collection
-    print(p.queries)    # gets queries in collection
-    print(p.tfidf)      # gets tfidf matrix using documents
+    bm25 = BM25Search(p.docs, p.qrels)
+    
+    # print(p.docs)       # gets documents in collection
+    # print(p.qrels)      # gets relevance in collection
+    # print(p.queries)    # gets queries in collection
+    # print(p.tfidf)      # gets tfidf matrix using documents
 
-
+    sorted_bm25 = bm25.getAPScores(p.queries)
+    print("====================================================")
+    print("BM25 Sorted :")
+    for q, ap in sorted_bm25:
+        print(f'{q}: {ap:.4f}')
+        
 if __name__ == "__main__":
     main()
