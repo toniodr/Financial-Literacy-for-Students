f4aa0ae50f8a7af4ae5a1ef431f83d4d918855ef
porterclev <porterclev@gmail.com>
2026-05-01 11:56:53 -0700

    usage example of search engine

--- main.py
+++ main.py
@@ -25,41 +25,55 @@ def main():
     unigram = LanguageModel(docs=p.docs, relevance=p.qrels, model='unigram', lambda_=0.3)
     bigram = LanguageModel(docs=p.docs, relevance=p.qrels, model='bigram', lambda_=0.3)
 
+
     ranked_docs = bm25.search(p.queries)
     sorted_bm25 = bm25.getAPScores()
     
     print("====================================================")
-    print("Ranked Indices per Query")
+    print("BM25")
     print(ranked_docs)    
-    print("====================================================")
-    print("BM25 Sorted :", end='\n\n')
     for q, ap in sorted_bm25:
         print(f'{q}: {ap:.4f}')
     
-    
-    # sorted_vsm = vsm.getAPScores(p.queries)
-    # print("====================================================")
-    # print("VSM Sorted :", end='\n\n')
-    # for q, ap in sorted_vsm:
-    #     print(f'{q}: {ap:.4f}')
-    
-    # sorted_bim = bim.getAPScores(p.queries)
-    # print("====================================================")
-    # print("BIM Sorted :", end='\n\n')
-    # for q, ap in sorted_bim:
-    #     print(f'{q}: {ap:.4f}')
-        
-    # sorted_unigram = unigram.getAPScores(p.queries)
-    # print("====================================================")
-    # print("Unigram Sorted :", end='\n\n')
-    # for q, ap in sorted_unigram:
-    #     print(f'{q}: {ap:.4f}')
 
-    # sorted_bigram = bigram.getAPScores(p.queries)
-    # print("====================================================")
-    # print("Bigram Sorted :", end='\n\n')
-    # for q, ap in sorted_bigram:
-    #     print(f'{q}: {ap:.4f}')
+    ranked_vsm = vsm.search(p.queries)
+    sorted_vsm = vsm.getAPScores()
+
+    print("====================================================")
+    print("VSM")   
+    print(ranked_vsm)
+    for q, ap in sorted_vsm:
+        print(f'{q}: {ap:.4f}')
+
+
+    ranked_bim = bim.search(p.queries)
+    sorted_bim = bim.getAPScores()
+
+    print("====================================================")
+    print("BIM")   
+    print(ranked_bim)
+    for q, ap in sorted_bim:
+        print(f'{q}: {ap:.4f}')
+
+
+    ranked_uni = unigram.search(p.queries)
+    sorted_unigram = unigram.getAPScores()
+
+    print("====================================================")
+    print("LM Unigram")   
+    print(ranked_uni)
+    for q, ap in sorted_unigram:
+        print(f'{q}: {ap:.4f}')
+
+
+    ranked_bi = bigram.search(p.queries)
+    sorted_bigram = bigram.getAPScores()
+
+    print("====================================================")
+    print("LM Bigram")   
+    print(ranked_bi)
+    for q, ap in sorted_bigram:
+        print(f'{q}: {ap:.4f}')
     
 if __name__ == "__main__":
     main()
