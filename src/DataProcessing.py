f43b8d39f4fdd851b2df2ca5d4357143b5c4fb2f
Antonio Duran <aduran1030@gmail.com>
2026-05-02 13:22:32 -0700

    added surface level normalization, standardize whitespace, and stemming using PorterStemmer

--- src/DataProcessing.py
+++ src/DataProcessing.py
@@ -1,9 +1,12 @@
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem import PorterStemmer
 from pathlib import Path
 import pandas as pd
 import json
 import re
+
+stemmer = PorterStemmer()
 
 def tokenizer(text):
     """Global Tokenize Method for bm25_search, bim_search, etc. Appears in both index and query process.
@@ -14,8 +17,23 @@ def tokenizer(text):
     Returns:
         _type_: _description_
     """
-    text = re.sub(r'[^\w\s]', '', text.lower())
-    return [t for t in text.split() if t not in ENGLISH_STOP_WORDS] 
+
+    # surface level normalization 
+    text = text.lower();
+
+    # remove punctuation 
+    text = re.sub(r'[^\w\s]', '', text)
+
+    # normalize white space to a single space 
+    text = re.sub(r'\s+', ' ', text).strip()
+
+    # tokenize and stopping
+    tokens = [t for t in text.split() if t not in ENGLISH_STOP_WORDS] 
+
+    # stemming
+    stemmed_tokens = [stemmer.stem(t) for t in tokens]
+
+    return stemmed_tokens
 
 class DataProcessing:
     def __init__(self, DATA_DIR="dataset"):
@@ -25,6 +43,7 @@ class DataProcessing:
             DATA_DIR (str, optional): Path to dataset directory. Defaults to "dataset".
         """
         data_dir = Path(DATA_DIR)
+
         with open(data_dir / "documents.json", encoding='utf-8') as f:
             self.docs = pd.DataFrame(json.load(f))
         
@@ -35,5 +54,6 @@ class DataProcessing:
             self.queries = pd.DataFrame(json.load(f))
 
         # Calculate tfidf cause we prob need it for whatever ranking we decide to do
-        self.vectorizer = TfidfVectorizer(stop_words='english')
+        # passed the tokenizer function as the tokenizer for tfidf vectorizer
+        self.vectorizer = TfidfVectorizer(stop_words='english', tokenizer=tokenizer, token_pattern=None)
         self.tfidf = self.vectorizer.fit_transform(self.docs['text'])
